d44ce4e903f42472cb1718f1951eff5f07ebbefd
kobabasu <keiji.kobayashi.web@gmail.com>
2015-08-24 12:58:58 -0700

    pathにsrcを追加する

--- layouts/admin/Header.jsx
+++ layouts/admin/Header.jsx
@@ -13,10 +13,10 @@ export default class Header extends React.Component {
         <p>Header</p>
 
         <ul>
-          <li><Link to="/sample/">front home</Link></li>
-          <li><Link to="/sample/sample">front sample</Link></li>
-          <li><Link to="/sample/admin">admin home</Link></li>
-          <li><Link to="/sample/count">admin count</Link></li>
+          <li><Link to="/src/sample/">front home</Link></li>
+          <li><Link to="/src/sample/sample">front sample</Link></li>
+          <li><Link to="/src/sample/admin">admin home</Link></li>
+          <li><Link to="/src/sample/count">admin count</Link></li>
         </ul>
 
         {this.props.children}
