1af5fd649bd8cacedebf5576d089ab753f1f4486
kobabasu <keiji.kobayashi.web@gmail.com>
2015-09-05 02:47:30 +0900

    改行を追加

--- layouts/front/Header.jsx
+++ layouts/front/Header.jsx
@@ -13,10 +13,14 @@ export default class Header extends React.Component {
         <p>Header</p>
 
         <ul>
-          <li><Link to="/src/sample/">front home</Link></li>
-          <li><Link to="/src/sample/sample">front sample</Link></li>
-          <li><Link to="/src/sample/admin">admin home</Link></li>
-          <li><Link to="/src/sample/count">admin count</Link></li>
+          <li><Link to="/src/sample/"
+            >front home</Link></li>
+          <li><Link to="/src/sample/sample"
+            >front sample</Link></li>
+          <li><Link to="/src/sample/admin"
+            >admin home</Link></li>
+          <li><Link to="/src/sample/count"
+            >admin count</Link></li>
         </ul>
 
       </section>
